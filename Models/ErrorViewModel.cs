2d028f482f13a75b234395ea8be56278e5df1cc4
vongo <vongo@LAPTOP-9GP7Q5TM>
2024-04-11 20:32:41 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Digigram.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
